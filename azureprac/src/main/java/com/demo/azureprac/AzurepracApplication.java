c4bd10ce521caf758469e670ef98168dab620b25
hp <hp@Sarveksha-Sai>
2022-02-19 15:43:10 +0530

    First Commit

---
+++ azureprac/src/main/java/com/demo/azureprac/AzurepracApplication.java
@@ -0,0 +1,13 @@
+package com.demo.azureprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurepracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurepracApplication.class, args);
+	}
+
+}
